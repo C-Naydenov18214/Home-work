01c9c65aaf74fae6d2ac8e03976a87657cb1f233
Sergey <tussss89@gmail.com>
2018-09-17 15:25:42 +0700

    Новое решение

--- Zabc2.hs
+++ Zabc2.hs
@@ -1,5 +1,9 @@
-{-# LANGUAGE FlexibleContexts #-}
-f a b c | a == 0 = error "Eto ne kvadratnoe"    
-        | b*b - 4 * a * c > 0 = (((-b - sqrt (b*b-4*a*c))/(2*a)), ((-b + sqrt (b*b-4*a*c))/(2*a))) 
-        | b*b - 4 * a * c == 0 = b/(2*a)
-        | b*b - 4 * a * c < 0 = error "Korney net" 
+korni a b c | (a == 0) && (b == 0) = error "ERROR" 
+            | (a == 0) && (b /= 0) = (x,x) 
+            | (a /= 0) = (x1,x2) 
+ where 
+        d = b*b-4*a*c 
+        sd = if d<0 then error "net korney" else sqrt d 
+        x1 = (-b-sd)/(2*a) 
+        x2 = (-b+sd)/(2*a) 
+        x = -c/b
