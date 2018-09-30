7c15ea000386317ced866ed69765abcaba699bfe
Sergey <tussss89@gmail.com>
2018-09-30 19:33:14 +0700

    Штраф с True

--- 2MLN.hs
+++ 2MLN.hs
@@ -1,7 +1,9 @@
-sumP a = sum (filter (\x -> prime x == 1 ) [1..a]) 
+sumP::Integer->Integer
+sumP a = sum (filter (\x -> prime x) [1..a]) 
  where
-  prime a = if  (a == 1)  then  0 else del a b  
-    where 
-     b = 2
-     del a b = if (b >= a) then 1 else  if (mod a b == 0) then 0 else del a (b + 1)
+  prime::Integer->Bool
+  prime a = if  (a == 1)  then  False else del a 2  
+   where 
+    del::Integer->Integer->Bool
+    del a b = if (b >= a) then True else  if (mod a b == 0) then False else del a (b + 1)
             
