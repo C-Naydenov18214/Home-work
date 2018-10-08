f6d3d1aa14beae0db0898151b481c51aac8f376d
Sergey <42936486+C-Naydenov18214@users.noreply.github.com>
2018-10-08 20:05:54 +0700

    Delete Zabc2.hs

--- Zabc2.hs
+++
@@ -1,9 +0,0 @@
-korni a b c | (a == 0) && (b == 0) = error "ERROR" 
-            | (a == 0) && (b /= 0) = (x,x) 
-            | (a /= 0) = (x1,x2) 
- where 
-        d = b*b-4*a*c 
-        sd = if d<0 then error "net korney" else sqrt d 
-        x1 = (-b-sd)/(2*a) 
-        x2 = (-b+sd)/(2*a) 
-        x = -c/b
