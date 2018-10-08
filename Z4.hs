77da1aacb63316d98472f28e9685897bc67bf35b
Sergey <42936486+C-Naydenov18214@users.noreply.github.com>
2018-10-08 20:05:01 +0700

    Delete Z4.hs

--- Z4.hs
+++
@@ -1,3 +0,0 @@
-chast a b = if ((a < 0) || (b < 0))  then error "ERROR"  else if (b == 0) then 0 else chas a b   
- where 
- 	chas a b   = if (b == 1) then a else if (a == b) then 1 else if (a < b ) then 0 else if ((a > 0) && (a - b >= 0)) then chas (a - b) (b) + 1 else a 
