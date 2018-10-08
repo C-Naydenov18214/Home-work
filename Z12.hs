4db8110810320cbeb83f7e405862ba920bd576f2
Sergey <42936486+C-Naydenov18214@users.noreply.github.com>
2018-10-08 20:04:01 +0700

    Delete Z12.hs

--- Z12.hs
+++
@@ -1,6 +0,0 @@
-nok a b = if (a == b) then a else pr a b 
- where
-  pr a b = (a * b) / (nod a b)
-   where	
-    nod a b = if (a == b) then a else if (a > b) then nod (a - b) b else if (a < b) then nod a (b - a) else nod a b  
-
