e2793f57223cdde959ee760695b55403621b9a0c
Sergey <42936486+C-Naydenov18214@users.noreply.github.com>
2018-10-08 20:04:31 +0700

    Delete Z16.hs

--- Z16.hs
+++
@@ -1,7 +0,0 @@
-del2 x1 x2 = operM (del x1 x2)
- where
-   del x1 x2 y = x1 <= x2 * y 
-   operM :: (Int -> Bool) -> Int 
-   operM r = operM1 0 
-    where 
-     operM1 y = if r y then y else operM1 (y+1)
