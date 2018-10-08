a77758025bf716ad030858036aca2446488caf08
Sergey <42936486+C-Naydenov18214@users.noreply.github.com>
2018-10-08 20:05:08 +0700

    Delete Z5.hs

--- Z5.hs
+++
@@ -1,3 +0,0 @@
-rest a b = if ((a < 0) || (b < 0))  then error "ERROR"  else if (b == 0) then 0 else res a b  
- where 
-        res a b  = if (a - b >= b) then res (a - b) b else if (a < b) then a else if (a == b) then 0 else a - b  
