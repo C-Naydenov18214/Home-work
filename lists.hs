901a901d5cdb169c08dca890802b57b690c208eb
Sergey <42936486+C-Naydenov18214@users.noreply.github.com>
2018-10-16 07:48:54 +0300

    LAB 1 переделанная

--- lists.hs
+++ lists.hs
@@ -1,67 +1,93 @@
----1)
+--1
+get [] n = []
 get (x:xs) n = if (n == 0) then  x else  get (xs) (n-1) 
 
---2)
+--2
+hea [] = error "EMPTY LIST"
 hea  (x:xs) = x
 
---3) last
-ls (x:xs) = if (xs == []) then  x else ls (xs)
+--3
+tail' [] = error "EMPTY LIST"
+tail' (x:xs) = xs
 
---4) tail
-f (x:xs) = xs 
+--4
+last' [] = error "EMPTY LIST"
+last' (x:[]) = x
+last' (x:xs) = last'(xs)  
 
---5)
- 
-ini [x] = []
-ini (x:xs) =  x:(ini(xs)) 	
---6)reverse
+--5
 rev [] = []
 rev (x:xs) = rev2 (x:xs) [] 
              where
-             	rev2 (x:xs) as = if (xs == []) then x:as else rev2 (xs) (x:as)
---7) length
-len [] = 0
-len (x:xs) = len (x:xs) 1
- where 
- 	len (x:xs) n = if (xs == []) then n else len (xs) (n+1)  
---8) app 
-app (x:xs) n = rev (n:rev(x:xs))
---9) con 
+             	rev2 (x:[]) as = x:as 
+                rev2 (x:xs) as = rev2 xs (x:as)
+
+--6
+len []  = 0
+len (x:xs) = len' (x:xs) 0
+len' [] n = n
+len' (x:xs) n = len' (xs) (n+1)  
+
+--7
+app (x:xs) n = app' (x:xs) n 
+ where
+   app' (x:[]) n = x:(n:[])                
+   app' (x:xs) n = x:(app (xs) n)  
+
+--8
 con::[a]->[a]->[a]
 con (x:[]) (ys) = x:ys
+con [] [] = error "EMPTY LIST"
 con (x:xs) (ys) = x:(con (xs) (ys))
---10) Drop
+
+--9
 dro::Integer -> [a] -> [a]
+dro  _ [] = []
 dro 0 xs = xs 
 dro n (x:xs) = dro (n-1) (xs) 
---11) take n xs
+
+--10
 take1::Integer->[a]->[a]
-ys = []
-take1 1 (x:xs) = x:ys
-take1 n (x:xs) = x:(take1 (n-1) (xs) )
---12)
+take1 _ [] = []
+take1 1 (x:xs) = x:[]
+take1 n (x:xs) = x:(take1 (n-1) (xs))
+
+--11
 split::Integer->[a]->([a],[a]) 
 split n [] = ([],[])
-split n (x:xs) = (take1 (n-1) (x:xs) , dro (n-1) (x:xs))
---13)
+split n xs = (take1 (n-1) (xs) , dro (n-1) (xs))
+
+
+--12
 nul::[a]->Bool
 nul [] =  True
 nul xs = False
---14)Elem
+
+--13
 el:: Eq a=>a->[a]->Bool
 el n [] = False
 el n (x:xs) = if (x == n) then True else el n (xs)
---15) filter test xs 
+
+--14
+
 fil::(a->Bool)->[a]->[a]
 fil test [] = []
 fil test (x:xs) = if (test (x) == True) then x:(fil test (xs)) else fil test (xs)
---16)
+
+--15
 mapp::(a->b)->[a]->[b]
 mapp f [] = []
 mapp f (x:xs) = f x:(mapp f xs)
---17) zip xs xy
+
+--16
 zipp::[a]->[b]->[(a,b)]
 zipp [] [] = []
 zipp [] xs = []
 zipp ys [] = []
 zipp (x:xs) (y:ys) = (x,y):(zipp (xs) (ys)) 
+
+--17
+
+init' [] = error "ERROR"
+init' (x:[]) = []
+init' (x:xs) =  x:(init xs) 
