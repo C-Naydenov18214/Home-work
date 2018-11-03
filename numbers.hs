d4373bbfb0a4d436e8ebd8e063e45ffb29c287cc
Sergey <42936486+C-Naydenov18214@users.noreply.github.com>
2018-11-03 20:35:41 +0700

    Add files via upload

--- numbers.hs
+++ numbers.hs
@@ -1,44 +1,42 @@
 import Data.Char
-toDecimal::Int->String->String
-toDecimal base snumber | ((base > 1) && (base <=61)) = perevoD2 base snumber 
-                       | (base == 1) = perevoDT snumber   
-                       | otherwise = error "NE VERNUE DANNIE"
+toDecimal::Integer->String->String
+toDecimal base snumber | ((base > 1) && (base <=61)) = translate base snumber 
+                       | (base == 1) = translateToThuring snumber   
+                       | otherwise = error "INVALID DATA (base > 61 or base < 1)"
  where
-  perevoD2::Int->String->String 
-  perevoD2 base snumber | any (\x -> (transf x) >= base) snumber  = error "ER1"   
-                        | otherwise = show (div (foldl (\sum x -> base * (sum+(transf x))) 0 snumber) base)
-                         where
-                           transf::Char->Int
-                           transf c | (ord '0' <= ord c) && (ord c <= ord '9') = ord c - ord '0'
-                                    | (ord 'a' <= ord c) && (ord c <= ord 'z') = ord c - 87
-                                    | (ord 'A' <= ord c) && (ord c <= ord 'Z') = ord c - 29                
-                                    | otherwise = error "ER2"
-  perevoDT::String->String
-  perevoDT snumber | all (== '1') snumber = show (length snumber - 1)              
-                   | otherwise = error "ERROR"
+  translate::Integer->String->String 
+  translate base snumber | any (\x -> (transf x) >= toInteger(base)) snumber  = error "INVALID DATA in the number some numeral >= base"   
+                         | otherwise = show ( foldl (\sum x -> base * sum+transf x) 0 snumber) 
+                          where
+                           transf::Char->Integer
+                           transf c | (ord '0' <= ord c) && (ord c <= ord '9') = toInteger(ord c - ord '0')
+                                    | (ord 'a' <= ord c) && (ord c <= ord 'z') = toInteger(ord c - 87)
+                                    | (ord 'A' <= ord c) && (ord c <= ord 'Z') = toInteger(ord c - 29 )               
+                                    | otherwise = error "INVALID DATA in the number some numeral >= base"
+  translateToThuring::String->String 
+  translateToThuring snumber | all (== '1') snumber = show (length snumber - 1)              
+                             | otherwise = error "INVALID DATA some numerdl > 1"
 
 
-fromDecimal::Int->String->String
-fromDecimal base snumber | (base >1) && (base <=61)= perevoD3 base (read snumber)
-                         | 1 == base = perevoDT snumber
-                         | otherwise = error "ERRORORORORORORORO"
+fromDecimal::Integer->String->String
+fromDecimal base snumber | (base >1) && (base <= 61) = translateFrom base (read snumber)
+                         | 1 == base = translateToThuring snumber
+                         | otherwise = error "INVALID DATA some numerdl > 1"
  where
-   perevoDT::String->String
-   perevoDT snumber = replicate (read snumber + 1) '1'
-   perevoD3::Int->Int->String
-   perevoD3 base 0  = ""
-   perevoD3 base snumber = perevoD3 base (div snumber base) ++ ([transfToChr (mod snumber base)])
+   translateToThuring::String->String
+   translateToThuring snumber = replicate (read snumber + 1) '1'
+   translateFrom::Integer->Integer->String
+   translateFrom base 0  = ""
+   translateFrom base snumber = translateFrom (base) (div (snumber) (base)) ++ ([transfToChr (mod  (snumber) (base))])
   
                     where  
-                     transfToChr::Int->Char
-                     transfToChr b | '0' <= chr (b +  ord '0') && chr (b + ord '0') <= '9' = chr (b + ord '0')
-                                   | 'a' <= chr (b + 87) && chr (b + 87) <= 'z' = chr (b + 87)
-                                   | 'A' <= chr (b + 29) && chr (b + 29 ) <= 'Z' = chr (b + 29) 
+                     transfToChr::Integer->Char
+                     transfToChr b | '0' <= chr (fromInteger (b + 48)) && chr (fromInteger (b + 48)) <= '9' = chr (fromInteger (b + 48))
+                                   | 'a' <= chr (fromInteger (b + 87)) &&  chr (fromInteger (b + 87)) <= 'z' = chr (fromInteger (b + 87))
+                                   | 'A' <= chr (fromInteger (b + 29)) &&  chr (fromInteger (b + 29 )) <= 'Z' = chr (fromInteger  (b + 29)) 
+                                   | otherwise = error "INVALID DATA base > 61 or base < 1"
 
-
-
-
-convertFromTo::Int->Int->String->String
+convertFromTo::Integer->Integer->String->String
 convertFromTo fr to snumber = fromDecimal to (toDecimal fr snumber)
                                                                 
                              	                       
