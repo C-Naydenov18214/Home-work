fcb2e8611cb643b6fb7ae7cc43f4229c31a58f44
Sergey <42936486+C-Naydenov18214@users.noreply.github.com>
2018-11-09 23:53:40 +0700

    Add files via upload

---
+++ MyHeap.hs
@@ -0,0 +1,29 @@
+import Data.List
+heapsort::[Int]->[Int]
+heapsort xs = heapRel xs (length xs)
+ where 
+ 	 heapRel::[Int]->Int->[Int]
+ 	 heapRel xs len | len == 1 = [head xs]
+ 	                | otherwise = sort ((fromInteger (div (toInteger(len)) 2 )) - 1) xs 
+ 	                  where 
+ 	                	 sort::Int->[Int]->[Int]
+ 	                	 sort len2 xs | len2 > -1 = sort (len2 - 1) (take right (take left ((take len2 xs) ++ [head midl] ++ (drop (len2 + 1) xs)) ++ [midl !! 1] ++ drop (left + 1) ((take len2 xs) ++ [head midl] ++ (drop (len2 + 1) xs))) ++ last1 ++ drop (right + 1) (take left ((take len2 xs) ++ [head midl] ++ (drop (len2 + 1) xs)) ++ [midl !! 1] ++ drop (left + 1) ((take len2 xs) ++ [head midl] ++ (drop (len2 + 1) xs )))) 
+ 	                	              | otherwise = end xs
+ 	                	               where
+ 	                	              	last1 = if (length (midl) == 2) then [] else [last midl]
+ 	                	              	   
+ 	                	              	midl = if (length xs) == right then swap (xs !! max) (xs !! left) else swap2 (xs !! max) (xs !! left) (xs !! right) 
+ 	                	              	    
+ 	                	              	      where
+ 	                	              	       swap::Int->Int->[Int]
+ 	                	              	       swap a b | a >= b = [a] ++ [b]
+ 	                	              	                | a <= b = [b] ++ [a]
+ 	                	              	       swap2::Int->Int->Int->[Int]
+ 	                	              	       swap2 a b c | (a >= b) && (a >= c) = [a] ++ [b] ++ [c] 
+ 	                	              	                   | (b >= a) && (b >= c) = [b] ++ [a] ++ [c]
+ 	                	              	                   | (c >= a) && (c >= b) = [c] ++ [a] ++ [b] 
+ 	                	                left = len2 * 2 + 1
+ 	                	              	right = len2 * 2 + 2
+ 	                	              	max = len2
+ 	                	              	end::[Int]->[Int]
+ 	                	                end xs = (heapRel  ([last xs] ++ drop 1 (init xs)) (len - 1)) ++ (take 1 xs)                       
